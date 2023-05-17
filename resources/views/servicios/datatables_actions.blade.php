578b9e840ae43246476e3c2c3f956f649c4240ba
Josue Martinez <m.josue3443@gmail.com>
2023-05-17 09:20:02 -0600

    Update modulo servicios

--- resources/views/servicios/datatables_actions.blade.php
+++ resources/views/servicios/datatables_actions.blade.php
@@ -1,17 +1,17 @@
 @can('Ver Servicios')
-    <a href="{{ route('servicios.show', $id) }}" data-toggle="tooltip" title="Ver" class='btn btn-icon btn-flat-secondary rounded-circle'>
+    <a href="{{ route('servicios.show', $id) }}" data-toggle="tooltip" title="Ver" class='btn btn-sm btn-outline-secondary'>
         <i class="fa fa-eye"></i>
     </a>
-@endcan
+    @endcan
 
 @can('Editar Servicios')
-    <a href="{{ route('servicios.edit', $id) }}" data-toggle="tooltip" title="Editar" class='btn btn-icon btn-flat-info rounded-circle'>
+    <a href="{{ route('servicios.edit', $id) }}" data-toggle="tooltip" title="Editar" class='btn btn-sm btn-outline-info'>
         <i class="fa fa-edit"></i>
     </a>
 @endcan
 
 @can('Eliminar Servicios')
-    <a href="#" onclick="deleteItemDt(this)" data-id="{{ $id }}" data-toggle="tooltip" title="Eliminar" class='btn btn-icon btn-flat-danger rounded-circle'>
+    <a href="#" onclick="deleteItemDt(this)" data-id="{{ $id }}" data-toggle="tooltip" title="Eliminar" class='btn btn-sm btn-outline-danger'>
         <i class="fa fa-trash-alt"></i>
     </a>
 
