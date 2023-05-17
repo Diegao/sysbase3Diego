96c6998bf284008bc8bf054f2535b782f478c878
Josue Martinez <m.josue3443@gmail.com>
2023-05-17 08:35:29 -0600

    mejora de plantilla

--- resources/views/tipoequipos/datatables_actions.blade.php
+++ resources/views/tipoequipos/datatables_actions.blade.php
@@ -1,17 +1,17 @@
 @can('Ver Tipoequipos')
-    <a href="{{ route('tipoequipos.show', $id) }}" data-toggle="tooltip" title="Ver" class='btn btn-icon btn-flat-secondary rounded-circle'>
+    <a href="{{ route('tipoequipos.show', $id) }}" data-toggle="tooltip" title="Ver" class='btn btn-sm btn-outline-secondary'>
         <i class="fa fa-eye"></i>
     </a>
-@endcan
+    @endcan
 
 @can('Editar Tipoequipos')
-    <a href="{{ route('tipoequipos.edit', $id) }}" data-toggle="tooltip" title="Editar" class='btn btn-icon btn-flat-info rounded-circle'>
+    <a href="{{ route('tipoequipos.edit', $id) }}" data-toggle="tooltip" title="Editar" class='btn btn-sm btn-outline-info'>
         <i class="fa fa-edit"></i>
     </a>
 @endcan
 
 @can('Eliminar Tipoequipos')
-    <a href="#" onclick="deleteItemDt(this)" data-id="{{ $id }}" data-toggle="tooltip" title="Eliminar" class='btn btn-icon btn-flat-danger rounded-circle'>
+    <a href="#" onclick="deleteItemDt(this)" data-id="{{ $id }}" data-toggle="tooltip" title="Eliminar" class='btn btn-sm btn-outline-danger'>
         <i class="fa fa-trash-alt"></i>
     </a>
 
